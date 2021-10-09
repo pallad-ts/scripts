0d460edb7996a55d6b41431ce2e5a9949c46a3ba
Łukasz Kużyński <lukasz.kuzynski@gmail.com>
2021-10-09 13:46:51 +0200

    * Updated jest to 27 * Added support for jest-junit

--- jest-preset.js
+++ jest-preset.js
@@ -1,33 +1,37 @@
 const {defaults: preset} = require('ts-jest/presets');
 module.exports = {
-    collectCoverage: true,
-    coverageDirectory: "coverage",
-    coveragePathIgnorePatterns: [
-        "/node_modules/"
-    ],
-    coverageReporters: [
-        "json",
-        "text",
-        "lcov",
-    ],
-    errorOnDeprecated: true,
-    moduleFileExtensions: [
-        "js",
-        "json",
-        "ts",
-    ],
-    roots: [
-        "<rootDir>/test"
-    ],
-    moduleNameMapper: {
-        '^@src/(.*)$': '<rootDir>/src/$1',
-    },
-    transform: preset.transform,
-    testEnvironment: "node",
-    testRegex: ".*Test.ts$",
-    globals: {
-        'ts-jest': {
-            tsconfig: '<rootDir>/test/tsconfig.json'
-        }
-    }
+	collectCoverage: true,
+	coverageDirectory: "coverage",
+	coveragePathIgnorePatterns: [
+		"/node_modules/"
+	],
+	coverageReporters: [
+		"json",
+		"text",
+		"lcov",
+	],
+	errorOnDeprecated: true,
+	moduleFileExtensions: [
+		"js",
+		"json",
+		"ts",
+	],
+	roots: [
+		"<rootDir>/test"
+	],
+	moduleNameMapper: {
+		'^@src/(.*)$': '<rootDir>/src/$1',
+	},
+	transform: preset.transform,
+	testEnvironment: "node",
+	testRegex: ".*Test.ts$",
+	globals: {
+		'ts-jest': {
+			tsconfig: '<rootDir>/test/tsconfig.json'
+		}
+	},
+	reporters: [
+		'default',
+		'jest-junit'
+	]
 };
